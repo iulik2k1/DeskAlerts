4712285c0b2efa4b211ef503e8c10af43c80157b
iulik <iulik@IULIK-PC>
2022-06-30 11:08:16 +0300

    update

--- DeskMessage/Form1.Designer.cs
+++ DeskMessage/Form1.Designer.cs
@@ -80,7 +80,7 @@
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Form1";
+            this.Text = "Desk Alert!";
             this.ResumeLayout(false);
             this.PerformLayout();
 
